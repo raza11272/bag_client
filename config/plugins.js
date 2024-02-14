f009d73374454607e534fb093d82e1b160260c90
Afanur Hasan <82591975+raza11272@users.noreply.github.com>
2024-02-14 13:52:56 +0600

    Update plugins.js
    config changes in apollo

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,15 @@
 module.exports = {
   //
   graphql: {
+    enabled: true,
     config: {
       endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
+      playgroundAlways: true,
+      defaultLimit: 10,
+      maxLimit: 20,
       apolloServer: {
-        tracing: false,
+        tracing: true,
       },
     },
   },
@@ -16,14 +17,15 @@ module.exports = {
 module.exports = {
   //
   graphql: {
+    enabled: true,
     config: {
       endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
+      playgroundAlways: true,
+      defaultLimit: 10,
+      maxLimit: 20,
       apolloServer: {
-        tracing: false,
+        tracing: true,
       },
     },
   },
